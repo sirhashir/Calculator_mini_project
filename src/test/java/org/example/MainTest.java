2f3f072c6d3393363dd8cfc52fcce287edb8701a
sirhashir <umairhashir89@gmail.com>
2023-03-22 23:38:34 +0530

    Update MainTest.java

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -5,55 +5,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
-    /**
-     * Method under test: {@link Main#calculator()}
-     */
+class MainTest 
+{
+    
     @Test
-    @Disabled("TODO: Complete this test")
-    void testCalculator() {
-        // TODO: Complete this test
-
-        // Arrange and Act
-        // TODO: Populate arranged inputs
+    @Disabled("Test")
+    void testCalculator() 
+    {
         Main.calculator();
-
-        // Assert
-        // TODO: Add assertions on result
     }
 
-    /**
-     * Method under test: {@link Main#getPow(double, double)}
-     */
+    
     @Test
-    void testGetPow() {
+    void testGetPow()
+    {
         assertEquals(1.0E10d, Main.getPow(10.0d, 10.0d));
         assertEquals(9.765625E-4d, Main.getPow(0.5d, 10.0d));
         assertEquals(9.765625E-4d, Main.getPow(-0.5d, 10.0d));
         assertEquals(Double.NaN, Main.getPow(Double.NaN, 10.0d));
     }
 
-    /**
-     * Method under test: {@link Main#getLog(double)}
-     */
+   
     @Test
-    void testGetLog() {
+    void testGetLog() 
+    {
         assertEquals(2.302585092994046d, Main.getLog(10.0d));
     }
 
-    /**
-     * Method under test: {@link Main#getFact(int)}
-     */
+    
     @Test
-    void testGetFact() {
+    void testGetFact()
+    {
         assertEquals(3628800, Main.getFact(10));
     }
 
-    /**
-     * Method under test: {@link Main#getSqrt(double)}
-     */
+   
     @Test
-    void testGetSqrt() {
+    void testGetSqrt() 
+    {
         assertEquals(3.1622776601683795d, Main.getSqrt(10.0d));
     }
 }
